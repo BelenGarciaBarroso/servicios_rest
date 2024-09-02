61804788816d76af84149a8f1f5f4f11f42989d6
manana <manana@A13E21>
2024-09-02 13:56:43 +0200

    02/09/2024

---
+++ 04_servicio_crud_cursos/src/main/java/service/CursosService.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Curso;
+
+public class CursosService {
+	
+	List<Curso> cursos= new ArrayList();
+	
+	public boolean alta (Curso curso) {
+		return c
+	}
+	
+
+}
